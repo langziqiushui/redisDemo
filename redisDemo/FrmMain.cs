f1c2af57d628a1474fcadd2c92e2dacd362b5c6d
cc <chenchi@qq.com>
2017-07-18 10:40:31 +0800

    cc commit

---
+++ redisDemo/FrmMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace redisDemo
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            HashOperator op = new HashOperator();
+            if (op.Set<string>("temp", "name", "秋水"))
+            {
+                string name = op.Get<string>("temp", "name");
+                MessageBox.Show(name);
+            }
+        }
+    }
+}
